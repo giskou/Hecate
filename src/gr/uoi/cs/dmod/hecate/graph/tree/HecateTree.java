7584d8a1eda2bf4d2b06519b6801c997749f752c
Giannis Skoulis <iskoulis@cs.uoi.gr>
2010-06-27 21:24:21 +0300

    added get path function

--- src/gr/uoi/cs/dmod/hecate/graph/tree/HecateTree.java
+++ src/gr/uoi/cs/dmod/hecate/graph/tree/HecateTree.java
@@ -1,6 +1,11 @@
 package gr.uoi.cs.dmod.hecate.graph.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JTree;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 @SuppressWarnings("serial")
 public class HecateTree extends JTree {
@@ -19,5 +24,18 @@ public class HecateTree extends JTree {
 		for (int i = getRowCount() - 1; i >=0; i--){
 			collapseRow(i);
 		}
+	}
+	
+	public TreePath getPath(TreeNode treeNode) {
+		List<Object> nodes = new ArrayList<Object>();
+		if (treeNode != null) {
+			nodes.add(treeNode);
+			treeNode = treeNode.getParent();
+			while (treeNode != null) {
+				nodes.add(0, treeNode);
+				treeNode = treeNode.getParent();
+			}
+		}
+		return nodes.isEmpty() ? null : new TreePath(nodes.toArray());
 	}
 }
