e403f43f3fb8b49729acb51a59d1a46ed56c35c5
Giannis Skoulis <iskoulis@cs.uoi.gr>
2010-07-07 11:06:14 +0300

    Key is now an implementation of SqlItem

--- src/gr/uoi/cs/dmod/hecate/sql/Key.java
+++ src/gr/uoi/cs/dmod/hecate/sql/Key.java
@@ -3,14 +3,28 @@ package gr.uoi.cs.dmod.hecate.sql;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Key {
+public class Key implements SqlItem{
+	String name;
 	TreeMap<String, Attribute> key;
+	private char mode;
 	
-	public Key(TreeMap<String, Attribute> k) {
+	public Key(String name, TreeMap<String, Attribute> k) {
 		this.key = k;
+		this.name = name;
 	}
 	
-	public String toString() {
+	@Override
+	public char getMode() {
+		return mode;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String print() {
 		String buff = new String();
 		buff = "Primary Key: ";
 		for (Map.Entry<String, Attribute> entry : this.key.entrySet()) {
@@ -19,5 +33,19 @@ public class Key {
 		}
 		buff += "\n";
 		return buff;
+	}
+
+	@Override
+	public void setMode(char c) {
+		mode = c;
+	}
+
+	@Override
+	public String whatAmI() {
+		return "Key";
+	}
+	
+	public String toString() {
+		return name;
 	}
 }
