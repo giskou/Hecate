004efef14ab4790c131b2930f4998e4551172fa3
Giannis Skoulis <iskoulis@cs.uoi.gr>
2013-10-02 17:14:30 +0300

    change map to linked hash to maintain insertion order

--- src/gr/uoi/cs/daintiness/hecate/io/TablesOverVersion.java
+++ src/gr/uoi/cs/daintiness/hecate/io/TablesOverVersion.java
@@ -7,8 +7,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 
 /**
  * @author iskoulis
@@ -16,21 +16,21 @@ import java.util.TreeMap;
  */
 public class TablesOverVersion {
 
-	private TreeMap<String, TreeMap<Integer, Integer>> tables;
+	private LinkedHashMap<String, LinkedHashMap<Integer, Integer>> tables;
 
 	public boolean addTable(String name, int version, int size) {
 		if (tables.containsKey(name)) {
 			tables.get(name).put(version, size);
 			return false;
 		} else {
-			tables.put(name, new TreeMap<Integer, Integer>());
+			tables.put(name, new LinkedHashMap<Integer, Integer>());
 			tables.get(name).put(version, size);
 			return true;
 		}
 	}
 	
 	public TablesOverVersion() {
-		this.tables = new TreeMap<String, TreeMap<Integer, Integer>>();
+		this.tables = new LinkedHashMap<String, LinkedHashMap<Integer, Integer>>();
 	}
 
 	public int getTableSize() {
@@ -40,7 +40,12 @@ public class TablesOverVersion {
 	public void export(String path, int versions) throws IOException {
 		String filePath = Export.getDir(path) + File.separator + "tables.csv";
 		BufferedWriter tables = new BufferedWriter(new FileWriter(filePath));
-		for (Entry<String, TreeMap<Integer, Integer>> e : this.tables.entrySet()) {
+		tables.write(" ;");
+		for (int i = 0; i < versions; i++) {
+			tables.write(i+1 + " ;");
+		}
+		tables.write("\n");
+		for (Entry<String, LinkedHashMap<Integer, Integer>> e : this.tables.entrySet()) {
 			tables.write(e.getKey() + ";");
 			for (int i = 0; i < versions; i++) {
 				if (e.getValue().containsKey(i)) {
